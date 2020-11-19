5b9a29d3ad2c6e10516b64df275e97a5ec3f52d8
pablito <pablito@amazon.com>
2020-11-19 13:19:08 -0800

    fix redirect uri issue

--- webapp/public/javascripts/qs_sa_app.js
+++ webapp/public/javascripts/qs_sa_app.js
@@ -1,6 +1,6 @@
 window.onload = (event) => {
-    const url = "https://" + window.location.host;
+    const url = "https://" + window.location.host + "/auth";
     document.getElementById('placeholder').innerHTML= url;
-    document.getElementById('slack_url').href += url + "/auth";
+    //document.getElementById('slack_url').href += url + "/auth";
 };
 
