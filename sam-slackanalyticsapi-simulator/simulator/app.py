af80456b15819dd58a7bfdbedc86ec6055e0c37a
pablito <pablito@amazon.com>
2020-12-04 17:55:19 -0800

    readme enhancements

--- sam-slackanalyticsapi-simulator/simulator/app.py
+++ sam-slackanalyticsapi-simulator/simulator/app.py
@@ -1,18 +1,22 @@
 import json
 import random
-import gzip
+import datetime
 
 
 # Sample lambda function to respond to test scenarios of the connector build
 def lambda_handler(event, context):
     
     print(event)
-    print("Executing DATA event...")
     request_date = event["queryStringParameters"]["date"]
+    print("Executing DATA event for {}...".format(request_date))
+
+    date = datetime.datetime.strptime(request_date, '%Y-%m-%d')
+    current_date = datetime.datetime.now()
+    day_diff = (current_date - date).days
     data_array = []
     
     for x in range(1000):
-        activity = random.randint(0, 60)
+        activity = get_random(day_diff)
         is_active = False
         if activity > 0:
             is_active = True
@@ -30,11 +34,11 @@ def lambda_handler(event, context):
             "is_active": is_active,
             "is_active_iOS": False,
             "is_active_Android": False,
-            "is_active_desktop": True,
+            "is_active_desktop": is_active,
             "reactions_added_count": random.randint(0, 20),
             "messages_posted_count": random.randint(0, 40),
             "channel_messages_posted_count": activity,
-            "files_added_count": 5
+            "files_added_count": random.randint(0, 3)
         })
     
     result = [json.dumps(record) for record in data_array]
@@ -46,3 +50,12 @@ def lambda_handler(event, context):
       "body": body
     }
 
+
+# Introduce a positive activity trend on mockup data.
+def get_random(day_diff):
+    random_int = random.randint(0, 100)
+    if day_diff > 20 and random_int < 60:
+        return 0
+    if day_diff > 10 and random_int < 20:
+        return 0
+    return random_int
